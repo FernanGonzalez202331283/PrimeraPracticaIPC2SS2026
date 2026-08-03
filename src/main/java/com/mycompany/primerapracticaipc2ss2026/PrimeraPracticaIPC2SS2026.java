673aab7930da7a7adaa8ab87da467cf2d745021a
Fernan Gonzalez <fernangonzalez202331283@cunoc.edu.gt>
2026-08-03 14:04:39 -0600

    creacion del proyecto

---
+++ src/main/java/com/mycompany/primerapracticaipc2ss2026/PrimeraPracticaIPC2SS2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primerapracticaipc2ss2026;
+
+/**
+ *
+ * @author fernan
+ */
+public class PrimeraPracticaIPC2SS2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
